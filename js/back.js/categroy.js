cea624382d5dbe6e222a7090f9ea801c43a12671
wangyong-0123 <1131589815@qq.com>
2019-12-29 18:43:58 +0800

    模板引用

--- js/back.js/categroy.js
+++ js/back.js/categroy.js
@@ -1,15 +1,21 @@
 var categroy = {
     show:function(callback) {
         $.get('http://localhost:8000/admin/category_search',function(res) {
-            console .log(res)
             callback(res);
         })
     },
-    add:function(callback) {
-        $.post('http://localhost:8000/admin/category_add',)
+    add:function(recipientName, recipientText) {
+        $.post('http://localhost:8000/admin/category_add',{name:recipientName,slug:recipientText},function(res) {
+            alert(res.msg);
+            if(res.code == 200){
+                location.href = 'article_category.html';
+            }
+        })
     },
-    delete:function(callback) {
-        $.post('http://localhost:8000/admin/category_delete',)
+    delete:function() {
+        $.post('http://localhost:8000/admin/category_delete',{id:id},function(res) {
+            console.log(res)
+        })
     },
     updata:function(callback) {
 
